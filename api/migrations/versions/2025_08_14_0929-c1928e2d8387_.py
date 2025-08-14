fc1e42f2853f55eb6df5e551ca5492a07afeef2c
zhangx1n <sjhpzx@gmail.com>
2025-08-14 18:30:02 +0800

    fix multiple heads

---
+++ api/migrations/versions/2025_08_14_0929-c1928e2d8387_.py
@@ -0,0 +1,25 @@
+"""empty message
+
+Revision ID: c1928e2d8387
+Revises: fa8b0fa6f407, e8446f481c1e
+Create Date: 2025-08-14 09:29:54.912128
+
+"""
+from alembic import op
+import models as models
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c1928e2d8387'
+down_revision = ('fa8b0fa6f407', 'e8446f481c1e')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
